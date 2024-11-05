9b87301f62d15f949540c7e838b0949162fde5bc
LONGICAL <763113963@qq.com>
2024-11-05 22:21:19 +0800

    Update

--- Key_Led/BSP/LED/inc/bsp_led.h
+++ Key_Led/BSP/LED/inc/bsp_led.h
@@ -47,7 +47,25 @@
 //********************* Queue_Handler ********************//
 
 //********************* Queue_Handler ********************//
+//****************** function resturn status  ************//
+typedef enum
+{
+    LED_OK = 0,               /* Operation completed successfully.  */
+    LED_ERROR = 1,            /* Run-time error without case matched*/
+    LED_ERRORTIMEOUT = 2,     /* Operation failed with timeout      */
+    LED_ERRORRESOURCE = 3,    /* Resource not available.            */
+    LED_ERRORPARAMETER = 4,   /* Parameter error.                   */
+    LED_ERRORNOMEMORY = 5,    /* Out of memory.                     */
+    LED_ERRORISR = 6,         /* Not allowed in ISR context         */
+    LED_RESERVED = 0x7FFFFFFF /* Reserved                           */
+} led_status_t;
 
+typedef enum
+{
+    LED_ON = 0,     /* Operation completed successfully.  */
+    LED_OFF = 1,    /* Operation completed successfully.  */
+    LED_TOGGLE = 2, /* Operation completed successfully.  */
+} led_operation_t;
 
 //******************************** Defines **********************************//
 #endif // BSP_LED_H
